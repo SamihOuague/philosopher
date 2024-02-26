b7b0bbe74ad9ddc2b04f76323eac215c99812edd
SamihOuague <ouaguenouni.samih@gmail.com>
2024-02-26 06:28:51 +0100

    Bonus Part working good git add *

--- philo_bonus/main.c
+++ philo_bonus/main.c
@@ -6,7 +6,7 @@
 /*   By: souaguen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 03:44:09 by souaguen          #+#    #+#             */
-/*   Updated: 2024/02/25 07:11:02 by souaguen         ###   ########.fr       */
+/*   Updated: 2024/02/26 06:09:57 by souaguen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@
 #include <string.h>
 #include <pthread.h>
 
-
 typedef struct s_list
 {
 	void			*content;
@@ -36,8 +35,10 @@ typedef struct s_philo
 	int	id;
 	int	n_fork;
 	pid_t	pid;
-	sem_t	**forks;
+	sem_t	*forks;
 	sem_t	*msg_lock;
+	sem_t	*fork_lock;
+	sem_t	*die_lock;
 	t_list	**msg_queue;
 	int	time_to_die;
 	int	time_to_eat;
@@ -163,7 +164,9 @@ void	send_msg(t_philo *self, int status)
 	char		*str;
 
 	str = "";
-	sem_wait((*self).msg_lock);	
+	if ((*(*self).die_lock).__align == 0)
+		return ;
+	sem_wait((*self).msg_lock);
 	t = get_timestamp_ms();
 	if (status == 1)
 		str = "has taken a fork";
@@ -202,7 +205,27 @@ int	precision_sleep(int time_to_sleep, t_philo *self)
 	return (0);
 }
 
-void	philo_proc(t_philo *philo, int index)
+int	wait_forks(t_philo *self)
+{
+	sem_t	*forks;
+
+	forks = (*self).forks;
+	if ((*forks).__align > (*self).n_fork)
+		return (1);
+	while ((*forks).__align < 2)
+	{
+		if ((get_timestamp_ms() - (*self).last_meal) > (*self).time_to_die)
+			break ;
+		usleep(1000);
+	}
+	sem_wait((*self).forks);
+	send_msg(self, 1);
+	sem_wait((*self).forks);
+	send_msg(self, 1);
+	return (0);
+}
+
+int	philo_proc(t_philo *philo, int index)
 {
 	pid_t	pid;
 	int		i;
@@ -215,15 +238,6 @@ void	philo_proc(t_philo *philo, int index)
 	self = &philo[index];
 	i = 0;
 	pid = fork();
-	memset(n, '\0', 6);
-	ft_strcpy(n, "fork0");
-	current_fork = (*self).id - 1;
-	next_fork = (*self).id % (*self).n_fork;
-	if (next_fork < current_fork)
-	{
-		current_fork = next_fork;
-		next_fork = (*self).id - 1;
-	}
 	if ((*self).id % 2 == 0)
 		usleep(1000);
 	if (pid == 0)
@@ -231,94 +245,63 @@ void	philo_proc(t_philo *philo, int index)
 		i = -1;
 		(*self).last_meal = get_timestamp_ms();
 		while ((++i) != (*self).n_time_eat)
-		{
+		{	
 			if ((get_timestamp_ms() - (*self).last_meal) > (*self).time_to_die)
 				break ;
 			send_msg(self, 0);
-			
-			if ((get_timestamp_ms() - (*self).last_meal) > (*self).time_to_die)
-				break ;
-			sem_wait((*self).forks[current_fork]);			
-			if ((get_timestamp_ms() - (*self).last_meal) > (*self).time_to_die)
-			{
-				sem_post((*self).forks[current_fork]);
-				break ;
-			}
-			send_msg(self, 1);
 			if ((*self).n_fork <= 1)
 			{
+				sem_wait((*self).forks);
+				send_msg(self, 1);
 				usleep((*self).time_to_die * 1000);	
-				sem_post((*self).forks[current_fork]);
+				sem_post((*self).forks);
 				break ;
-			}
-			sem_wait((*self).forks[next_fork]);
-			if ((get_timestamp_ms() - (*self).last_meal) > (*self).time_to_die)
-			{
-				sem_post((*self).forks[current_fork]);	
-				sem_post((*self).forks[next_fork]);
+			}	
+			sem_wait((*self).fork_lock);
+			if ((*(*self).die_lock).__align == 0)
 				break ;
-			}
+			if (wait_forks(self))
+				break ;
+			sem_post((*self).fork_lock);
 
-			send_msg(self, 1);
 			if ((get_timestamp_ms() - (*self).last_meal) > (*self).time_to_die)
-			{
-				sem_post((*self).forks[current_fork]);	
-				sem_post((*self).forks[next_fork]);
 				break ;
-			}
-			
 
 			(*self).last_meal = get_timestamp_ms();
 
 			send_msg(self, 2);	
 			if (precision_sleep((*self).time_to_eat, self))
 				break ;
+			
 			if ((get_timestamp_ms() - (*self).last_meal) > (*self).time_to_die)
-			{
-				sem_post((*self).forks[current_fork]);	
-				sem_post((*self).forks[next_fork]);
 				break ;	
-			}
 			send_msg(self, 3);
-			sem_post((*self).forks[current_fork]);
-			sem_post((*self).forks[next_fork]);
 			if ((get_timestamp_ms() - (*self).last_meal) > (*self).time_to_die)
-				break ;
+				break ;	
+			sem_post((*self).forks);
+			sem_post((*self).forks);
 			if (precision_sleep((*self).time_to_sleep, self))
 				break ;
 		}
+		sem_post((*self).fork_lock);
+		sem_post((*self).forks);
+		sem_post((*self).forks);
 		exit_code = 0;	
 		if (i != (*self).n_time_eat)
 			exit_code = (*self).id;
-		i = -1;
-		while ((++i) < (*self).n_fork)
-		{
-			n[4] = '1' + i;
-			sem_close((*self).forks[i]);
-			sem_unlink(n);	
-		}
-		free((*self).forks);
+		sem_close((*self).forks);
+		sem_unlink("forks1");
 		free((*self).args);
 		sem_close((*self).msg_lock);
 		sem_unlink("msg_lock");
+		sem_close((*self).fork_lock);
+		sem_unlink("fork_lock");	
+		sem_close((*self).die_lock);
+		sem_unlink("die_lock");
 		free(philo);
 		exit(exit_code);
 	}
-	return ;
-}
-
-void	*monitor_routine(void *arg)
-{
-	t_monitor	*self;
-
-	self = arg;
-	while (1)
-	{
-		printf("%p\n", *(*self).msg_queue);
-	
-		usleep(100000);
-	}
-	return (NULL);
+	return (pid);
 }
 
 
@@ -378,6 +361,7 @@ int	*check_args(int argc, char **argv)
 	return (args);
 }
 
+
 int	main(int argc, char **argv)
 {	
 	unsigned long	t;
@@ -385,13 +369,16 @@ int	main(int argc, char **argv)
 	int	i;
 	char	n[6];
 	char	*name;
-	sem_t	**forks;
+	sem_t	*forks;
 	sem_t	*msg_sem;
+	sem_t	*fork_sem;
+	sem_t	*die_sem;
 	t_philo	*philo;
 	int	*args;
 	int	deadbeef;
 	int	exit_status;
 	t_monitor	monitor;
+	pid_t		pid;
 
 	if (!(argc == 5 || argc == 6))
 		return (1);
@@ -404,20 +391,13 @@ int	main(int argc, char **argv)
 	args = check_args(argc, argv);
 	philo = init_philo(args);
 	if (philo == NULL)
-		return (1);
-	forks = malloc(sizeof(sem_t *) * args[0]);
-	if (forks == NULL)
-		return (1);
-	while ((++i) < args[0])
-	{
-		name = malloc(sizeof(char) * 6);
-		memset(name, '\0', 6);
-		n[4] = '1' + i;
-		ft_strcpy(name, n);
-		sem_unlink(name);	
-		forks[i] = sem_open(name, O_CREAT, 0666, 1);	
-		free(name);
-	}	
+		return (1);	
+	sem_unlink("forks1");	
+	sem_unlink("fork_lock");	
+	sem_unlink("die_lock");
+	forks = sem_open("forks1", O_CREAT, 0666, args[0]);
+	fork_sem = sem_open("fork_lock", O_CREAT, 0666, 1);
+	die_sem = sem_open("die_lock", O_CREAT, 0666, 1);
 	i = -1;
 	if (philo == NULL)
 		return (1);
@@ -425,37 +405,38 @@ int	main(int argc, char **argv)
 	msg_sem = sem_open("msg_lock", O_CREAT, 0666, 1);	
 	while ((++i) < args[0])
 	{
-		philo[i].id = i + 1; 	
+		philo[i].id = i + 1;
 		philo[i].forks = forks;
 		philo[i].msg_lock = msg_sem;
 		philo[i].started_at = started_at;
-		philo_proc(philo, i);
+		philo[i].fork_lock = fork_sem;
+		philo[i].die_lock = die_sem;
+		if (philo_proc(philo, i) == 0)
+			break ;
 	}
 	i = -1;
 	while ((++i) < args[0])
-	{
-		waitpid(-1, &exit_status, 0);
-		sem_wait(msg_sem);
+	{	
+		pid = waitpid(-1, &exit_status, 0);	
 		if (WEXITSTATUS(exit_status) != 0 && !deadbeef)
 		{
+			sem_wait(msg_sem);
 			printf("%ld %d died\n", get_timestamp_ms() - started_at, WEXITSTATUS(exit_status));
 			deadbeef = 1;
-			kill(-1, 0);
-			break ;
+			sem_post(msg_sem);
+			sem_wait(die_sem);
 		}
-		sem_post(msg_sem);
 	}
 	i = -1;
-	while ((++i) < args[0])
-	{
-		n[4] = '1' + i;
-		sem_close(forks[i]);
-		sem_unlink(n);	
-	}
-	free(args);
+	sem_close(forks);
+	sem_unlink("forks1");
 	sem_close(msg_sem);
 	sem_unlink("msg_lock");
-	free(forks);
+	sem_close(fork_sem);
+	sem_unlink("fork_lock");
+	sem_close(die_sem);
+	sem_unlink("die_lock");
+	free(args);
 	free(philo);
 	return (0);
 }
